caad0105df5634207e2930458b25394c7f7574fb
root <root@srv819718.hstgr.cloud>
2025-08-16 00:08:05 +0000

    latest

--- evcsms/csms/permissions.py
+++ evcsms/csms/permissions.py
@@ -8,12 +8,24 @@ class IsCustomer(BasePermission):
 class IsCpAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_cp_admin
-"""
-class IsRootAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_root_admin
-"""
+
 
 class IsRootAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == "root"
+
+
+
+class IsNormalUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and getattr(request.user, "is_normal_user", False)
+
+class IsSuperAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and getattr(request.user, "is_super_admin", False)
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return request.user.is_authenticated  # any logged-in user can read
+        return request.user.is_authenticated and getattr(request.user, "is_super_admin", False)
